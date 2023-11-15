be924df78bc7e2b43567bd5b0dd7f73943990407
DHieucter <24122003Hieu>
2023-11-15 15:12:15 +0700

    Code View quản lý bài viết

---
+++ FPLSP_Tutorial.WASM/Pages/QuanLyBaiViet/ViewModels/PostListVM.cs
@@ -0,0 +1,13 @@
+﻿using FPLSP_Tutorial.Application.DataTransferObjects.Tag;
+
+namespace FPLSP_Tutorial.WASM.Pages.QuanLyBaiViet.ViewModels
+{
+    public class PostListVM
+    {
+        public Guid? Id { get; set; }
+        public string? Title { get; set; }
+        public List<TagDto> tagDtos { get; set; }
+        public string? Type { get; set; }
+        public int? STT { get; set; } = 0;
+    }
+}

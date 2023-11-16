0749e7691c0ce5303c074d658eeae693659a3c8c
hoangpdph31561 <hoangpdph31561@fpt.edu.vn>
2023-11-16 16:48:01 +0700

    update base solution

--- FPLSP_Tutorial.Application/DataTransferObjects/Example/Request/ViewExampleWithPaginationRequest.cs
+++ FPLSP_Tutorial.Application/DataTransferObjects/Example/Request/ViewExampleWithPaginationRequest.cs
@@ -1,8 +1,14 @@
 ﻿using FPLSP_Tutorial.Application.ValueObjects.Pagination;
+using FPLSP_Tutorial.Domain.Enums;
 
 namespace FPLSP_Tutorial.Application.DataTransferObjects.Example.Request
 {
     public class ViewExampleWithPaginationRequest : PaginationRequest
     {
+        // SearchFields
+        public string? Name { get; set; }
+
+        // SortFields
+        public EntityStatus? Status { get; set; }
     }
 }

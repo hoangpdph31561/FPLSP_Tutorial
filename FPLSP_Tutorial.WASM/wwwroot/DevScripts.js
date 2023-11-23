9d19f5afb8533fc32b3ee00dfdb6811a683cc392
N1N1 <N1N1@DESKTOP-VB5UE44>
2023-11-23 13:36:44 +0700

    add rich text editor

---
+++ FPLSP_Tutorial.WASM/wwwroot/DevScripts.js
@@ -0,0 +1,11 @@
+﻿function CreateRTEInstance() { 
+    window.editor1 = new RichTextEditor("#inp_editor1");
+}
+
+function SetRTEValue(value) {
+    editor1.setHTMLCode(value);
+}
+
+function GetRTEValue(value) { 
+    return editor1.getHTMLCode();
+}

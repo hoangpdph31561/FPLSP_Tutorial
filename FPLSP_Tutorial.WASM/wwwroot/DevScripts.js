7afdb5d064ecf148bc0c69c0cd5a6bc0e460baf2
N1N1 <N1N1@DESKTOP-VB5UE44>
2023-11-28 00:42:11 +0700

    ReArr

--- FPLSP_Tutorial.WASM/wwwroot/DevScripts.js
+++ FPLSP_Tutorial.WASM/wwwroot/DevScripts.js
@@ -1,5 +1,7 @@
 ﻿function CreateRTEInstance() { 
-    window.editor1 = new RichTextEditor("#inp_editor1");
+    var config = {}
+    config.editorResizeMode = "none";
+    window.editor1 = new RichTextEditor("#inp_editor1", config);
 }
 
 function SetRTEValue(value) {
@@ -8,4 +10,14 @@ function SetRTEValue(value) {
 
 function GetRTEValue(value) { 
     return editor1.getHTMLCode();
+}
+
+function ReadClipboard() {
+    navigator.clipboard.readText()
+        .then(function (clipText) {
+            alert("Clipboard content: " + clipText);
+        })
+        .catch(function (error) {
+            console.error("Error reading clipboard:", error);
+        });
 }

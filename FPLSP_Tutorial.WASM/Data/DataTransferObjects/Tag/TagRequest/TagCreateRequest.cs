9c3026df1003edba583f878936bbb027740b78a5
N1N1 <N1N1@DESKTOP-VB5UE44>
2023-12-17 13:56:46 +0700

    Tag validation

--- FPLSP_Tutorial.WASM/Data/DataTransferObjects/Tag/TagRequest/TagCreateRequest.cs
+++ FPLSP_Tutorial.WASM/Data/DataTransferObjects/Tag/TagRequest/TagCreateRequest.cs
@@ -1,10 +1,14 @@
 ﻿using FPLSP_Tutorial.WASM.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace FPLSP_Tutorial.WASM.Data.DataTransferObjects.Tag.TagRequest
 {
     public class TagCreateRequest
     {
         public Guid? MajorId { get; set; }
+
+        [Required(ErrorMessage = "Chưa nhập tên nhãn")]
+        [MaxLength(10, ErrorMessage = "Không nhập quá 10 ký tự")]
         public string Name { get; set; } = string.Empty;
         public EntityStatus Status { get; set; } = EntityStatus.Active;
 
